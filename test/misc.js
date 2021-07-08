5c35868c365999fb72be1d106cb6c58b76095df7
Eddie Kohler <ekohler@gmail.com>
2021-07-08 15:20:04 -0400

    hotcrp: Add "hotcrp" preset.

--- test/misc.js
+++ test/misc.js
@@ -445,7 +445,7 @@ describe('Ordered list info', function () {
 
 
 describe('HotCRP', function () {
-  var md = markdownit({ hotcrp: true });
+  var md = markdownit('hotcrp');
 
   it('Should handle a missing backtick', function () {
     assert.strictEqual(
