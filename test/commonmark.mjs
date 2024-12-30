27917c5b980a1e2c48b4879f34452f611fb50672
Eddie Kohler <ekohler@gmail.com>
2024-12-30 09:47:37 -0800

    Handle `\r` and `\r\n` sequences in the parser
    Rather than normalizing \r and \r\n to \n. The purpose is to keep
    the original string unchanged, eventually allowing positions in the
    input string to be passed through to the output.

--- test/commonmark.mjs
+++ test/commonmark.mjs
@@ -5,10 +5,10 @@ import markdownit from '../index.mjs'
 import { assert } from 'chai'
 
 function normalize (text) {
-  return text.replace(/<blockquote>\n<\/blockquote>/g, '<blockquote></blockquote>')
+  return text.replace(/<blockquote>(?:\r\n?|\n)<\/blockquote>/g, '<blockquote></blockquote>')
 }
 
-function generate (path, md) {
+function generate (path, md, nlmode) {
   load(path, function (data) {
     data.meta = data.meta || {}
 
@@ -16,8 +16,23 @@ function generate (path, md) {
 
     (data.meta.skip ? describe.skip : describe)(desc, function () {
       data.fixtures.forEach(function (fixture) {
+        let first = fixture.first.text
+        const second = fixture.second.text
+        if (nlmode > 0) {
+          if (!/\n/.test(first)) {
+            return
+          } else if (nlmode === 1) {
+            first = first.replace(/\n/g, '\r')
+          } else {
+            first = first.replace(/\n/g, '\r\n')
+          }
+        }
         it(fixture.header ? fixture.header : 'line ' + (fixture.first.range[0] - 1), function () {
-          assert.strictEqual(md.render(fixture.first.text), normalize(fixture.second.text))
+          let result = md.render(first)
+          if (nlmode > 0) {
+            result = result.replace(/\r\n?/g, '\n')
+          }
+          assert.strictEqual(result, normalize(second))
         })
       })
     })
@@ -27,5 +42,17 @@ function generate (path, md) {
 describe('CommonMark', function () {
   const md = markdownit('commonmark')
 
-  generate(fileURLToPath(new URL('fixtures/commonmark/good.txt', import.meta.url)), md)
+  generate(fileURLToPath(new URL('fixtures/commonmark/good.txt', import.meta.url)), md, 0)
+})
+
+describe('CommonMark with CR', function () {
+  const md = markdownit('commonmark')
+
+  generate(fileURLToPath(new URL('fixtures/commonmark/good.txt', import.meta.url)), md, 1)
+})
+
+describe('CommonMark with CRNL', function () {
+  const md = markdownit('commonmark')
+
+  generate(fileURLToPath(new URL('fixtures/commonmark/good.txt', import.meta.url)), md, 2)
 })
